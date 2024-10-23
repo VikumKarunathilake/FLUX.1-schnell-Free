b4c691b4bddd442b502580965504c3d54589d65a
VikumKarunathilake <vikumbk@yahoo.com>
2024-10-23 08:34:06 +0530

    .

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ with gr.Blocks() as demo:
 
 if __name__ == "__main__":
     init_db()  # Initialize the database on program start
-    demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
